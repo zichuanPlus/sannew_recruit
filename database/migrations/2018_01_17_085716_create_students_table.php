742c095004c4617b61fa2218cbb79a95fb9f656b
king <yilunvren@163.com>
2018-01-18 09:51:07 +0800

    v1.1.1
    数据迁移

---
+++ database/migrations/2018_01_17_085716_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',60);
+            $table->string('idcard_number',20);
+            $table->string('phone',11);
+            $table->tinyInteger('admit_status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
